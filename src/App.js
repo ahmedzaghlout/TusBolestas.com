21306eb19fccdcd078d126e8f5c33fee97c921da
ahmed zaghlout <84550594+ahmedzaghlout@users.noreply.github.com>
2022-11-28 20:22:24 -0500

    base

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+ 
+
+class App extends React.Component {
+
+    constructor(props) {
+        super()
+    }
+
+
+    render() {
+        return (
+            
+            <Router>
+                <Routes>
+ 
+                </Routes>
+            </Router>
+        )
+    }
+}
+
+export default App
